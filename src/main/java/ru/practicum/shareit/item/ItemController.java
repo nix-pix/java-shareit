f375171762e5310ffcf953a036a4b5160bd2cdeb
nix-pix <nix-pix@yandex.ru>
2023-05-10 22:37:10 +0300

    Переписал ItemController

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -5,20 +5,25 @@ import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemAllDto;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.request.ItemRequestService;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/items")
 @AllArgsConstructor
 public class ItemController {
+    private final ItemRequestService itemRequestService;
     private final ItemService itemService;
 
     @PostMapping()
     public ItemDto save(@RequestHeader(value = "X-Sharer-User-Id") Long userId,
-                        @RequestBody @Valid ItemDto itemDto) {
-        return itemService.save(itemDto, userId);
+                        @RequestBody ItemDto itemDto) {
+        ItemRequestDto itemRequestDto = itemDto.getRequestId() != null
+                ? itemRequestService.getItemRequestById(itemDto.getRequestId(), userId)
+                : null;
+        return itemService.save(itemDto, itemRequestDto, userId);
     }
 
     @PatchMapping("/{itemId}")
@@ -35,19 +40,24 @@ public class ItemController {
     }
 
     @GetMapping()
-    public List<ItemAllDto> getAllItems(@RequestHeader(value = "X-Sharer-User-Id") Long userId) {
-        return itemService.getAll(userId);
+    public List<ItemAllDto> getAllItems(@RequestHeader(value = "X-Sharer-User-Id") Long userId,
+                                        @RequestParam(required = false) Integer from,
+                                        @RequestParam(required = false) Integer size) {
+        return itemService.getAll(userId, from, size);
     }
 
     @GetMapping("/search")
-    public List<ItemDto> search(@RequestParam String text) {
-        return itemService.search(text.toLowerCase());
+    public List<ItemDto> search(@RequestHeader(value = "X-Sharer-User-Id") Long userId,
+                                @RequestParam String text,
+                                @RequestParam(required = false) Integer from,
+                                @RequestParam(required = false) Integer size) {
+        return itemService.getByText(text.toLowerCase(), userId, from, size);
     }
 
     @PostMapping("{itemId}/comment")
-    public CommentDto createComment(@RequestBody @Valid CommentDto commentDto,
+    public CommentDto createComment(@RequestBody CommentDto commentDto,
                                     @PathVariable Long itemId,
-                                    @RequestHeader(value = "X-Sharer-User-Id")
+                                    @RequestHeader(value = "X-Sharer-User-Id", required = false)
                                     Long userId) {
         return itemService.createComment(commentDto, itemId, userId);
     }
